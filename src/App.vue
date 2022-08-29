01b63098850de5b4db2b5588caaed2944df6c5af
dai <8526238+dai25572236638@user.noreply.gitee.com>
2022-08-29 10:09:14 +0800

    main

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div ref="countref">{{ count }}</div>
+  <img alt="Vue logo" src="./assets/logo.png" @click="click">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script setup>
+import { ref, nextTick } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const count = ref(0)
+const countref = ref(null)
+
+const click = () => {
+  for (let i = 0; i < 10; i++) {
+    // nextTick(() => {
+    //   console.log(count.value);
+    //   console.log(countref.value.innerHTML);
+    // })
+  }
+  count.value = 10
+  console.log(countref.value.innerText);
+  nextTick(() => {
+    console.log(countref.value.innerHTML);
+  })
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
